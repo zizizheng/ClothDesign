b7c0277685e96322de2c4485f853a269d3c4e258
zizizheng <z0931752096@gmail.com>
2016-12-26 15:13:11 +0800

    prototype

--- src/app/show-cloth/show-cloth.component.ts
+++ src/app/show-cloth/show-cloth.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowClothComponent implements OnInit {
 
+  frontName: string = '成功大學';
+  number: string = '13';
+  clothColor: string = '#d143dc';
+  textColor: string = '#000000';
+  fontArray = [ '標楷體', '華康雅風體W3'];
+  selectFont = 'Yu Gothic';
+
   constructor() { }
 
   ngOnInit() {
